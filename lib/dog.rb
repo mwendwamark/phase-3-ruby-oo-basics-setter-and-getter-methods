81b547f710d4c82338d3b7b8bdb7377c0f719d26
mark mwendwa <mwendwamark310@gmail.com>
2023-05-17 11:37:19 +0300

    completed assingment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,27 @@
 class Dog
+    # Setter Methods 
+    def name=(value)
+        @name = value
+    end
+
+    def breed=(type)
+        @breed= type
+    end
+
+    # Getter Methods
+    def name 
+        @name
+    end    
+
+    def breed 
+        @breed
+    end
+
 
 end
+
+fido = Dog.new
+snoopy = Dog.new
+
+fido.name = "Fido"
+snoopy.breed = "Beagle"
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,25 @@
 class Person
 
+    # Setter Methods
+    def name=(value)
+        @name=value
+    end
+
+    def job=(type_of_job)
+        @job=type_of_job
+    end
+
+    # Getter Methods
+    def name 
+        @name
+    end
+
+    def job
+        @job
+    end
 end
+
+beyonce = Person.new
+
+beyonce.name = "Beyonce"
+beyonce.job = "Singer"
